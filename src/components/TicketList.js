05548bea2c9ff12e84e6d9e4a01610373de7424e
PavelEremeev <pe19950520@yandex.ru>
2022-04-25 23:07:42 +0300

    Добавлена кнопка подгрузки в TicketList

--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,19 +1,20 @@
-
-
 import React from 'react'
 import Ticket from './Ticket'
+
+import './TicketList.css'
 
 const TicketList = (props) => {
 	const { flights } = props
 
 	return (
-		<div className="ticket-list_wrapper">
+		<div className='ticket-list_wrapper'>
 			{flights.map((flight) =>
 				<Ticket
 					key={flight.flightToken}
 					{...flight}
 				/>
 			)}
+			<button className='ticket-list__button'>Показать ещё</button>
 		</div>
 	)
 }
